91a1db03cbf2e2f076f39cc1cd55564cb471b281
Yang ChungFan <sonic.tw.tp@gmail.com>
2018-11-14 16:30:03 +0900

    Split loader and actual OS image into 2 files, shrink size and can be loaded independtly

--- tools/embedded.sh
+++ tools/embedded.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 set -e
 
-map_addr=`readelf -a $1.elf | grep system_map | awk '{print $2}'`
+map_addr=`readelf -a $1.elf | grep system_map | awk '{print $2}' | tr '[:lower:]' '[:upper:]'`
 echo $map_addr
-map_addr_dec=`printf "%d" "0x$map_addr"`
+adj_map_addr=`echo "obase=16; ibase=16; $map_addr-20000000" | bc`
+echo $adj_map_addr
+map_addr_dec=`printf "%d" "0x$adj_map_addr"`
 dd if=$2 of=$1.sysmap.bin bs=1 seek=$map_addr_dec oflag=seek_bytes conv=notrunc
