81ccf3955f8602c69fea9f3c9d7e368d95eea624
Yang ChungFan <sonic.tw.tp@gmail.com>
2018-12-18 18:43:28 +0900

    Move timing related call to tux_timing, implelemted tux_gettimeofday

--- arch/x86_64/src/linux_subsystem/tux_timing.c
+++ arch/x86_64/src/linux_subsystem/tux_timing.c
@@ -1,3 +1,4 @@
+#include <sys/time.h>
 #include <nuttx/arch.h>
 #include <nuttx/sched.h>
 
@@ -11,3 +12,6 @@ int tux_nanosleep(unsigned long nbr, const struct timespec *rqtp, struct timespe
   return nanosleep(rqtp, rmtp);
 }
 
+int tux_gettimeofday(unsigned long nbr, struct timeval *tv, struct timeval *tz){
+  return gettimeofday(tv, tz);
+}
