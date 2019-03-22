ded0372ddf114fc4b4a5097cc9f367e9a5b4da6b
Yang ChungFan <sonic.tw.tp@gmail.com>
2019-03-22 17:31:23 +0900

    make a physical - virtual 1:1 mapping for native processes

--- arch/x86_64/src/intel64/up_initialstate.c
+++ arch/x86_64/src/intel64/up_initialstate.c
@@ -99,12 +99,19 @@ void up_initial_state(struct tcb_s *tcb)
    */
 
   if (rtcb != NULL && !up_interrupt_context()){
-    for(int i = 0; i < 128; i ++){
-      xcp->page_table[i] = rtcb->xcp.page_table[i];
-    }
-    xcp->is_linux = (rtcb->xcp.is_linux ? 1 : 0);
-    xcp->linux_sock = rtcb->xcp.linux_sock;
-    xcp->linux_tcb = rtcb->xcp.linux_tcb;
+      if(rtcb->xcp.is_linux){
+        for(int i = 0; i < 128; i ++){
+          xcp->page_table[i] = rtcb->xcp.page_table[i];
+        }
+        xcp->is_linux = 1;
+        xcp->linux_sock = rtcb->xcp.linux_sock;
+        xcp->linux_tcb = rtcb->xcp.linux_tcb;
+      }else{
+        for(int i = 0; i < 128; i ++){
+          xcp->page_table[i] = (0x200000 * i) | 0x83;
+        }
+        xcp->is_linux = 0;
+      }
   }else{
     for(int i = 0; i < 128; i ++){
       xcp->page_table[i] = (0x200000 * i) | 0x83;
