5dc9d776542b76df60df36ddaf66c2aeb8b293ef
Yang ChungFan <sonic.tw.tp@gmail.com>
2019-11-07 16:56:02 +0900

    Make 99xx operate at a higher frequency, allowing baud rate > 115200

--- configs/jailhouse-intel64/src/mcs99xx.c
+++ configs/jailhouse-intel64/src/mcs99xx.c
@@ -9,18 +9,107 @@
 #include <errno.h>
 #include <sched.h>
 
+#include <arch/board/virtio_ring.h>
+
 #include <arch/io.h>
 #include <nuttx/pcie/pcie.h>
+#include <nuttx/serial/uart_16550.h>
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 
 #define MAX_MCS99xx_DEV 4
+
+// These are for 16C950
+#define UART_ICR_OFFSET UART_LSR_OFFSET
+#define UART_EFR_OFFSET UART_IIR_OFFSET
+
+#define UART_ACR_OFFSET 0x00
+#define UART_CPR_OFFSET 0x01
+#define UART_TCR_OFFSET 0x02
+#define UART_CKS_OFFSET 0x03
+#define UART_TTL_OFFSET 0x04
+#define UART_RTL_OFFSET 0x05
+
+#define UART_EFR_ECB   0b00010000
+
+#define UART_ACR_ASRE  0b10000000
+#define UART_ACR_ICRRD 0b01000000
+#define UART_ACR_TLE   0b00100000
+
+// MCS99xx specific
+#define SER_VEN_REG         (0x204)
+#define SER_SOFT_RESET_REG  (0x238)
+#define SP_CLK_SELECT_REG   (0x214)
 
 /****************************************************************************
  * ivshmem: Fileops Prototypes and Structures
  ****************************************************************************/
 
 static int mcs99xx_count;
+
+// Helper Functions
+//
+
+#define UART_ACR_CONFIG (UART_ACR_TLE)
+#define UART_FIFO_DEPTH (16)
+
+//helper function for IO type read
+static inline uint8_t serial_in(uint8_t* membase, int offset)
+{
+  uint8_t tmp1;
+  uint8_t* mem = membase + 0x280 + (offset * 4);
+  tmp1 = READ_ONCE(*mem);
+  return tmp1;
+}
+
+//helper function for IO type write
+static inline void serial_out(uint8_t* membase, int offset, uint8_t value)
+{
+  uint8_t* mem = membase + 0x280 + (offset * 4);
+  WRITE_ONCE(*mem, value);
+}
+
+//Helper function to write to index control register
+static void serial_icr_write(uint8_t* membase, int offset, uint8_t value)
+{
+  serial_out(membase, UART_SCR_OFFSET, offset);
+  serial_out(membase, UART_ICR_OFFSET, value);
+}
+
+//Helper function to read from index control register
+static unsigned int serial_icr_read(uint8_t* membase, int offset)
+{
+  unsigned int value;
+  serial_icr_write(membase, UART_ACR_OFFSET, UART_ACR_CONFIG | UART_ACR_ICRRD);
+  serial_out(membase, UART_SCR_OFFSET, offset);
+  value = serial_in(membase, UART_ICR_OFFSET);
+  serial_icr_write(membase, UART_ACR_OFFSET, UART_ACR_CONFIG);
+  return value;
+}
+
+//Helper function to set the enhance mode
+void setserial_enhance_mode(uint8_t* membase)
+{
+  uint8_t lcr, efr;
+
+  lcr = serial_in(membase, UART_LCR_OFFSET);
+  serial_out(membase, UART_LCR_OFFSET, 0xBF);
+
+  efr = serial_in(membase, UART_EFR_OFFSET);
+  efr |= UART_EFR_ECB;
+  serial_out(membase, UART_EFR_OFFSET, efr);
+
+  serial_out(membase, UART_LCR_OFFSET, lcr);
+}
+
+// Helper function to clear the FIFO and disable it
+static inline void serial_clear_fifos(uint8_t* membase)
+{
+  serial_out(membase, UART_FCR_OFFSET, UART_FCR_FIFOEN);
+  serial_out(membase, UART_FCR_OFFSET, UART_FCR_FIFOEN |
+           UART_FCR_RXRST | UART_FCR_TXRST);
+  serial_out(membase, UART_FCR_OFFSET, 0);
+}
 
 /****************************************************************************
  * Initialize device
@@ -31,6 +120,7 @@ void mcs99xx_probe(uint16_t bdf)
   void* bar1mem;
   int cap;
   int pmcsr;
+  uint32_t ser_ven_val;
 
   if (pci_find_cap(bdf, PCI_CAP_MSI) < 0)
     {
@@ -54,6 +144,49 @@ void mcs99xx_probe(uint16_t bdf)
   pci_enable_device(bdf, (PCI_CMD_MASTER | PCI_CMD_MEM));
 
   pci_msi_set_vector(bdf, IRQ7 + mcs99xx_count++);
+
+  uint32_t* ssrr_mem = bar1mem + SER_SOFT_RESET_REG;
+  WRITE_ONCE(*ssrr_mem, 0x01);
+  serial_clear_fifos(bar1mem);
+  setserial_enhance_mode(bar1mem);
+
+  //Setting the FIFO trigger Levels
+  serial_icr_write(bar1mem, UART_RTL_OFFSET, UART_FIFO_DEPTH);
+  serial_icr_write(bar1mem, UART_TTL_OFFSET, UART_FIFO_DEPTH);
+  serial_icr_write(bar1mem, UART_ACR_OFFSET, UART_ACR_CONFIG);
+
+
+  uint32_t* svr_mem = bar1mem + SER_VEN_REG;
+  uint32_t* sclr_mem = bar1mem + SP_CLK_SELECT_REG;
+  if(mcs99xx_count == 1)
+    {
+      // This is the console, for the sake of USB serial transciver, use a standard baud */
+      // Set the device clock
+      ser_ven_val = READ_ONCE(*svr_mem);
+      ser_ven_val = 0;
+      WRITE_ONCE(*svr_mem, ser_ven_val);
+
+      // 14745600 Hz
+      ser_ven_val |= 0x50;
+      WRITE_ONCE(*svr_mem, ser_ven_val);
+      // Enable pre-scaling for high clock rate
+      WRITE_ONCE(*sclr_mem, 0);
+    }
+  else
+    {
+      // Others Use a non standard clock for high transmission rate */
+      // Set the device clock
+      ser_ven_val = READ_ONCE(*svr_mem);
+      ser_ven_val = 0;
+      WRITE_ONCE(*svr_mem, ser_ven_val);
+
+      // 48000000 Hz
+      ser_ven_val |= 0x70;
+      WRITE_ONCE(*svr_mem, ser_ven_val);
+      // Enable pre-scaling for high clock rate
+      WRITE_ONCE(*sclr_mem, 0);
+    }
+
 }
 
 struct pcie_dev_t pci_mcs99xx = {
